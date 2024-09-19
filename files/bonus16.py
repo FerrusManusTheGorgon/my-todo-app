6c81afcdeeeea79321c860b2b91e4d197bd1fa42
sean-young <syoung467@yahoo.com>
2024-09-18 22:31:58 -0700

    Add todos.txt and requirements.txt

---
+++ files/bonus16.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as sg
+from  zip_extractor import extract_archive
+
+
+label1 = sg.Text("Select files to compress:")
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose", key="files")
+
+label2 = sg.Text("Select destination folder: ")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
+
+
+extract_button = sg.Button("Extract")
+output_label = sg.Text(key="output", text_color="green")
+
+window = sg.Window("Archive Extractor",
+                   layout=[[label1, input1, choose_button1],
+                           [label2, input2, choose_button2],
+                           [extract_button, output_label]])
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    archivepath = values["archive"]
+    dest_dir = values["folder"]
+    extract_archive(archivepath, dest_dir)
+    window["output"].update(value="Extraction Completed!")
+
+window.close()
